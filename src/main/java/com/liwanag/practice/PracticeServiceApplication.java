faf0b835cf3f236a60d703eae39a362f4f71c19e
Jared Velasquez <jaredvel25@gmail.com>
2025-09-10 20:58:31 -0700

    feat: base for hexagonal architecture

--- src/main/java/com/liwanag/practice/PracticeServiceApplication.java
+++ src/main/java/com/liwanag/practice/PracticeServiceApplication.java
@@ -3,6 +3,22 @@ package com.liwanag.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Note: instead of the classic controller-service-repository Spring setup, I've chosen to structure
+// the Practice microservice with Hexagonal Architecture (Ports and Adapters).
+
+// I have chosen this architecture based on two factors:
+// 1. there are multiple external integrations (e.g. DynamoDB, S3) and I want to swap them out
+// with mocks for easier unit testing
+// 2. classic architecture (controller directly invokes service) is brittle to changes, esp. when new architecture
+// and features are added like ML answer evaluation or question generation
+
+// Hexagonal Architecture solves the above issues by isolating core business logic from databases or external APIs;
+// logic is independent of technological choices. Additionally, as long as adapters adhere to the defined
+// ports/interfaces, adapters can be swapped out easily for business logic to invoke.
+
+// Sources:
+// https://dev.to/jhonifaber/hexagonal-architecture-or-port-adapters-23ed
+// https://www.baeldung.com/hexagonal-architecture-ddd-spring
 @SpringBootApplication
 public class PracticeServiceApplication {
 
