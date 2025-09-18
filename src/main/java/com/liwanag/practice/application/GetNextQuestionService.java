348106aa63684cc9ccb631d7bfa688aed9acd6b6
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2025-09-17 23:17:13 -0700

    feat: add scaffold for claimNext

--- src/main/java/com/liwanag/practice/application/GetNextQuestionService.java
+++ src/main/java/com/liwanag/practice/application/GetNextQuestionService.java
@@ -15,18 +15,50 @@ import java.util.UUID;
 @Slf4j
 public class GetNextQuestionService implements GetNextQuestion {
     private final SessionStore sessionStore;
+    private final Integer LEASE_DURATION_SECONDS = 30;
 
     @Override
     public ClaimNext claimNext(UUID sessionId, UUID userId) {
+        // Three cases when calling claimNext:
+        // 1. session is inactive since all questions have been answered/skipped
+        // 2. session is active and the user has already been served a question (lease is active)
+        // 3. session is active and the user has not been served a question since starting / answering
+        // previous question (no active lease)
+
         Session session = sessionStore.load(sessionId, userId).orElseThrow(() -> new NoSuchElementException("Session not found"));
 
+        // case 1: session is inactive
         if (session.getCurrentIndex() >= session.getTotalQuestions()) {
             if (session.getStatus() != Session.Status.FINISHED) {
                 session.completeIfDone(true, Instant.now());
 
                 // TODO: then save session
             }
+
+            // TODO: return ClaimNext here
         }
+
+        // case 2: session is active and there's an active lease
+        Instant now = Instant.now();
+        if (session.getTurnToken() != null && session.getLeaseExpiresAt() != null && session.getLeaseExpiresAt().isAfter(now)) {
+            // TODO: serve the same question again
+
+            // TODO: then save session
+
+            // TODO: return ClaimNext here
+        }
+
+        UUID newAttemptId = UUID.randomUUID();
+        UUID newTurnToken = UUID.randomUUID();
+        Instant newLeaseExpiry = now.plusSeconds(LEASE_DURATION_SECONDS);
+
+        session.openAttempt(newAttemptId, newTurnToken, newLeaseExpiry);
+
+        // TODO: serve the next question
+
+        // TODO: then save session
+
+        // TODO: return ClaimNext here
 
         return null;
     }
